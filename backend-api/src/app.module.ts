020e72c81ad5f8e809c25e6af903233d776483a6
rikmyk <rikomaykel01@gmail.com>
2026-06-04 22:02:01 +0700

    feat: initialize App module with TypeORM and Config module integration

--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -1,3 +1,4 @@
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
