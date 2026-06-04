f84fc5eb8103c9b777431a06224ee782b4ca320e
rikmyk <rikomaykel01@gmail.com>
2026-06-04 22:02:26 +0700

    feat: initialize NestJS application with global prefix, CORS, and validation pipe configuration

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const port = configService.get<number>('PORT', 3001);
+  await app.listen(port);
+  console.log(`NestJS Application is running on: http://localhost:${port}/api`);
 }
 bootstrap();
